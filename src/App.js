929857df89121fb45153120f6f5e376ec8cba770
SauceCode <66637877+KODEHOUDINI@users.noreply.github.com>
2023-02-24 00:00:43 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./styles.css";
+
+
+export default function App() {
+  return (
+    <Canvas>
+
+    </Canvas>
+  );
+}
---
+++ src/Experience.js
@@ -0,0 +1,9 @@
+import Model from "./Shoe-draco";
+import { Perf } from "r3f-perf";
+import "./styles.css";
+
+export default function App() {
+  return {
+    /* <primitive */
+  };
+}
